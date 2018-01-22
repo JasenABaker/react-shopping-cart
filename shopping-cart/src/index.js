7ceb9f939369f82e42c3b71f6bf70796984f9bb8
JasenABaker <JasenABaker@gmail.com>
2018-01-21 20:57:51 -0500

    Added provider and app js to index js

--- shopping-cart/src/index.js
+++ shopping-cart/src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+
+
+import App from './components/App'
+import Store from './Store'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const StoreInstance = Store()
+
+ReactDOM.render(
+    <Provider store={StoreInstance}>
+    <App />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
