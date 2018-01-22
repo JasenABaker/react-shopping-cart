40acce2d96fef4880512a3bca729db3e52f79015
JasenABaker <JasenABaker@gmail.com>
2018-01-21 21:00:27 -0500

    Added chrome redux devetools

--- shopping-cart/src/Store.js
+++ shopping-cart/src/Store.js
@@ -3,5 +3,9 @@ import rootReducer from './reducers/RootReducer'
 
 
 export default (initialState)=> {
-    return createStore(rootReducer, initialState)
+    return createStore(
+        rootReducer, 
+        initialState,
+        window._REDUX_DEVTOOLS_EXTENSION_ && window._REDUX_DEVTOOLS_EXTENSION_()
+    )
 }
